672059e1b1c8b9c8a2209d1a8bcde3ddacd6c894
Thibaut <Thibaut@Thibaut-PC>
2014-02-18 17:56:38 +0100

    third commit

---
+++ EtudeDeCas/EtudeDeCas-ejb/src/java/manager/projetManager.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package manager;
+
+import entite.Projet;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Thibaut
+ */
+@Stateless
+@LocalBean
+public class projetManager {
+    
+    Projet monProjet = new Projet();
+    
+    public void nouveauProjet(Date dateDebut, Date dateFin, String description){
+        monProjet.setDateDebut(dateDebut);
+        monProjet.setDateFin(dateFin);
+        monProjet.setDescription(description);
+    }
+    
+    public void supprimerProjet(){
+
+    }
+    
+    public void modifierProjet(){
+        
+    }
+}
